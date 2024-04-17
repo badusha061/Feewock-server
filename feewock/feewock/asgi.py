18c31d7b6df41b00a21c3afa81a6522a71427f9b
badusha061 <badubadusha061@gmail.com>
2024-04-17 08:55:40 +0530

    change route

--- feewock/feewock/asgi.py
+++ feewock/feewock/asgi.py
@@ -1,16 +1,32 @@
-"""
-ASGI config for feewock project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feewock.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.consumers import TextConsumer , NoficationEmployee , NoficationUser
+from django.urls import re_path
+from channels.auth import AuthMiddleware , SessionMiddleware , AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feewock.settings')
 
-application = get_asgi_application()
+
+websocket_urlpatterns = [
+    re_path(r'^ws/chat/(?P<room_name>[\w_]+)/$', TextConsumer.as_asgi()),
+    re_path(r'^ws/notification/(?P<employee_id>\w+)/$', NoficationEmployee.as_asgi()),
+    re_path(r'^ws/notificationuser/(?P<userId>\w+)/$', NoficationUser.as_asgi()),
+]
+
+
+
+application = ProtocolTypeRouter({
+    "http" : get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )        
+}
+)
